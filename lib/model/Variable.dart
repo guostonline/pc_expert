38fb70d81237b723df40f4d5458e2191543f8e22
guostonline <48313334+guostonline@users.noreply.github.com>
2020-10-12 20:59:10 +0100

    last mod

--- lib/model/Variable.dart
+++ lib/model/Variable.dart
@@ -12,14 +12,4 @@ class MesVariable {
   bool visibleToshiba = false;
 
   bool visible1 = false;
-  bool visible2 = false;
-  bool visible3 = false;
-  bool visible4 = false;
-  bool visible5 = false;
-  bool visible6 = false;
-  bool visible7 = false;
-  bool visible8 = false;
-  bool visible9 = false;
-  bool visible10 = false;
-  bool visible11 = false;
 }
